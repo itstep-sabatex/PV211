f737e6cb9321181c59cfff791d97ea5a8b6683c2
sabatex <serhiy.lakas@hotmail.com>
2024-04-04 21:44:11 +0300

    *

--- ADOConnectedDemo/Program.cs
+++ ADOConnectedDemo/Program.cs
@@ -10,14 +10,46 @@ var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirector
 using (var connection = new Microsoft.Data.Sqlite.SqliteConnection(config.GetConnectionString("DefaultConnection")))
 {
     connection.Open();
+
     var dbCommand = connection.CreateCommand();
+    dbCommand.CommandText = "insert into role (Name) values (\"New user\")";
+    await dbCommand.ExecuteNonQueryAsync();
+
+    var transaction = connection.BeginTransaction();
+
+    dbCommand = connection.CreateCommand();
+    dbCommand.Transaction = transaction;
     dbCommand.CommandText = "select * from role";
-    var reader = dbCommand.ExecuteReader();
+    var reader = await dbCommand.ExecuteReaderAsync();
+    int i = 1;
     while (reader.Read())
     {
         var Id = reader.GetInt32("id");
         var name = reader.GetString("name");
         Console.WriteLine($"Id:{Id}   Name:{name}");
+        var c2 = connection.CreateCommand();
+        c2.Transaction = transaction;
+        c2.CommandText = $"update role set name=@name where id=@id";// string.Format("update role set name=\"{0}\" where id={1}", name, Id);
+        
+        var pId = c2.CreateParameter();
+        pId.ParameterName = "id";
+        pId.Value   = Id;
+
+        var pName = c2.CreateParameter();
+        pName.ParameterName = "name";
+        pName.Value = $"{name}-{i}";
+
+
+        await c2.ExecuteNonQueryAsync();
     }
+    //transaction.Commit();
+    transaction.Rollback();
+
+
+    dbCommand = connection.CreateCommand();
+    dbCommand.CommandText = "select count(id) from role";
+    var count = Convert.ToInt32(await dbCommand.ExecuteScalarAsync());
+    Console.WriteLine($"Records count: {count}");
+ 
 
 }
